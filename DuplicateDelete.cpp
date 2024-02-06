e471f9cb8452dec17dc25db7362afb9bac23d3c1
Sergey Belousov <sergeybeliyus@yandex.ru>
2024-02-06 10:18:42 +0500

    Initial Commit

---
+++ DuplicateDelete.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+	int count;
+	std::set<int, std::greater<int>> data;
+	std::cout << "Введите количество вводимых данных: ";
+	std::cin >> count;
+	for (size_t i = 0; i < count; i++) {
+		int number;
+		std::cout << i + 1 << ": ";
+		std::cin >> number;
+		data.insert(number);
+	}
+	for (const auto elem : data) {
+		std::cout << elem << std::endl;
+	}
+}
